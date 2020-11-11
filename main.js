c9529fc89f351dd3d23845e932cc5156121229f1
Alberto <thebertsinfo@gmail.com>
2020-11-11 12:20:33 +0100

    Bonus: Aggiungo elemento al keyup del tasto Enter

--- main.js
+++ main.js
@@ -19,6 +19,13 @@ var app = new Vue({
         //Creo funzione che al click di un elemento di item_list venga rimosso.
         removeItem(index){
             this.$delete(this.item_list, index);
+        },
+        //Creo funzione per aggiungere elemento ad array item_list alla pressione del tasto Enter
+        addOnEnter(e){
+            //keyCode 13 è l'id del tasto Enter
+            if (e.keyCode === 13) {
+                this.item_list.push(this.new_item);
+            }
         }
-    },
+    }
 });
